6a6c5733e4b163553748a21a6c8b98541ae66f12
Saul Espinoza <saul_zeul@hotmail.com>
2018-07-17 23:07:15 -0500

    Servidor ExpressJS y Rails funcionando

---
+++ app/javascript/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <pre> {{ $data }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Consumiendo API Vue ",
+      products: []
+    }
+  },
+    mounted() {
+    axios.get("http://localhost:4000/api/kindles.json").then(response => {
+      this.products = response.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+p, h2 {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
